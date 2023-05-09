6e8adfcf456a69316c9312bbb0ab11ec19ba6242
Antony26111996 <114063448+Antony26111996@users.noreply.github.com>
2023-05-09 12:16:12 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
-import React from "react";
-import "./style.css";
+// import logo from './logo.svg';
+import './App.css';
+import Blog from './components/blog';
+// import Head from './components/head';
+import Post from './components/post';
+import Side from './components/side';
+import Single from './components/single';
+import Write from './write';
+import Setting from './setting';
+import Login from './login';
+import Register from './register';
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
+import Home from './components/home'
 
-export default function App() {
+import Raj from './components/raj';
+import Singlepost from './singlepost';
+import Jsx from './components/jsx';
+import Complan from './components/complan';
+import Tional from './components/tional';
+import Lock from './components/lock';
+import Mapfunction from './components/mapfunction';
+import Rendering from './components/rendering';
+import Styleto from './components/styleto'
+import Reactrouter from './components/reactrouter';
+import Apireact from './components/apireact';
+import Asynchronous from './components/asynchronous'
+import Async from './components/async'
+import Hooksreact from './components/hooksreact';
+import Useeffect from './components/useeffect';
+function App() {
+  
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+  
+    
+    <div className="App">
+    
+      
+
+       <BrowserRouter>
+      
+       <Home/>
+        <Routes>
+        
+        <Route  path='/home' element={<Home />} /> 
+         <Route exact path='/blog' element={<Blog />} /> 
+          <Route  path='/side' element={<Side />} /> 
+          <Route  path='/singlepost' element={<Singlepost />} /> 
+          <Route path='/raj' element={<Raj />} /> 
+          <Route exact path='/setting' element={<Setting />} />
+           <Route  path='/login'  element={<Login />} /> 
+          <Route  path='/register' element={<Register />} /> 
+          <Route  path='/post' element={<Post />} />
+          <Route  path='/jsx' element={<Jsx />} /> 
+          <Route  path='/complan' element={<Complan />} /> 
+          <Route  path='/tional' element={<Tional />} /> 
+          <Route  path='/lock' element={<Lock/>} /> 
+          <Route  path='/mapfunction' element={<Mapfunction/>} /> 
+          <Route  path='/rendering' element={<Rendering/>} /> 
+          <Route  path='/styleto' element={<Styleto/>} /> 
+          <Route  path='/reactrouter' element={<Reactrouter/>} /> 
+          <Route  path='/apireact' element={<Apireact/>} /> 
+          <Route  path='/Asynchronous' element={<Asynchronous/>} /> 
+          <Route  path='/Async' element={<Async/>} /> 
+          <Route  path='/hooksreact' element={<Hooksreact/>} /> 
+          <Route  path='/useeffect' element={<Useeffect/>} /> 
+
+
+         </Routes>
+      </BrowserRouter> 
+
     </div>
   );
 }
+
+export default App;
